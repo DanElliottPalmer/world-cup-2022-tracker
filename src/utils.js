3595d1ab11a5bae778ab683c1e564f7504848f50
danelliottpalmer <danelliottpalmer@gmail.com>
2022-11-23 10:42:32 -0500

    feat(project): basic react + parcel

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+const URL_RESULTS =
+  "https://www.theguardian.com/football/world-cup-2022/results";
+const URL_FIXTURES =
+  "https://www.theguardian.com/football/world-cup-2022/fixtures";
+
+// export function
